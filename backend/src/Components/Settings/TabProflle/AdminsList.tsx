3d6b433810bb5d2c245e593c91efb1bfc3ed5790
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2024-11-13 10:22:39 +0530

    admin delete

--- backend/src/Components/Settings/TabProflle/AdminsList.tsx
+++ backend/src/Components/Settings/TabProflle/AdminsList.tsx
@@ -12,24 +12,50 @@ const AdminsList = () => {
   const [admins, setAdmins] = useState<AdminValues[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const fetchAdmins = async () => {
+    try {
+      const response = await fetch("/api/auth/get-all-admins", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await response.json();
+
+      setAdmins(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching admins:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchAdmins = async () => {
-      try {
-        const response = await fetch("/api/auth/get-all-admins", {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-        });
-
-        const data = await response.json();
-
-        setAdmins(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching admins:", error);
-      }
-    };
+    
     fetchAdmins();
   }, []);
+
+
+const handleDelete = async (row: any) => {
+  try {
+
+    const response = await fetch(`/api/auth/delete-admins/${row._id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      const error = await response.text();
+      throw new Error(error);
+    }
+
+    // toast.success("Admin deleted successfully");
+    // Refresh the admin list
+    await fetchAdmins();
+    return true;
+  } catch (error) {
+    console.error("Error deleting admin:", error);
+    // toast.error("Error deleting admin");
+    return false;
+  }
+};
 
   const columns = ["_id", "image", "name", "role", "email", "phone_number"];
 
@@ -57,6 +83,8 @@ const AdminsList = () => {
             loading={loading}
             pageSize={10}
             pagination={true}
+            onDelete={handleDelete}
+            isDelete={true}
             class="-striped -highlight"
             onClickField="first_name"
             handleOnClick={(row: any) =>
@@ -70,3 +98,8 @@ const AdminsList = () => {
 };
 
 export default AdminsList;
+
+
+
+
+
